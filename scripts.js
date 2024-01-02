0ffdb146755ce5321c406456a40a50d23169dae9
MichaelNeely77 <michael@michaelneely.net>
2024-01-02 11:59:49 -0500

    Added the calcualtor functions

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const addFunction = function(num1, num2) {
+    return num1 + num2;
+}
+
+const subFunction = function(num1, num2) {
+    return num1 - num2;
+}
+const multFunction = function(num1, num2) {
+    return num1 * num2;
+}
+const divFunction = (num1, num2) => {
+    return num1 / num2;
+}
